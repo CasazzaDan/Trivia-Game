d85936368c8d7af2c72296f25bee7c92ba4f5e11
DCas973 <dan.casazza@endeveran.com>
2019-02-04 23:41:25 -0600

    tried completing js but keep hitting walls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,98 @@
+$(document).ready(function() {
 // Variables
+    var correct = 0;
+    var incorrect = 0;
+    var currentQuestion = 0;
+    var answer = [];
+    
+
+    var trvia = [
+        q1 = {
+			question: "What is the elemental symbol for mercury?",
+			correct: 2,
+			multChoice: ['Me', 'Mc', 'Hg', 'Hy']
+		},
+		q2 = {
+			question: "What is the name of the main healing item in Dark Souls?",
+			correct: 1,
+			multChoice: ["Health Potion", "Estus Flask", "Orange Juice", "Ashen Flask"]
+		},
+		q3 = {
+			question: "What country saw a world record 315 million voters turn out for elections on May 20, 1991?",
+			correct: 3,
+			multChoice: ["United States of America", "Soviet Union", "Poland", "India"]
+		},
+		q4 = {
+			question: "What name does the little headcrab in &quot;Half Life 2&quot; have?",
+			correct: 0,
+			multChoice: ["Lamarr", "Jumperr", "Drett", "Jerry"]
+		},
+		q5 = {
+			question: "Which Nation DID NOT have a Colony in Modern-day America?",
+			correct: 0,
+			multChoice: ["Portugal", "Spain", "Sweden", "Netherlands"]
+		},
+		q6 = {
+			question: "Which of the following is not a character in the Street Fighter series?",
+			correct: 3,
+			multChoice: ["Laura Matsuda", "Sakura Kasugano", "Ibuki", "Mai Shiranui"]
+		},
+		q7 = {
+			question: "In &quot;One Piece&quot;, what does &quot;the Pirate King&quot; mean to the captain of the Straw Hat Pirates?",
+			correct: 1,
+			multChoice: ["Promise", "Freedom", "Adventure", "Friendship"]
+		},
+		q8 = {
+			question: "When Batman trolls the online chat rooms, what alias does he use?",
+			correct: 2,
+			multChoice: ["iAmBatman", "BWayne13", "JonDoe297", "BW1129"]
+        }
+    ];
+
 
 //=====================================================
-// Use AJAX you connect to the trivia API
-var queryURL = "https://opentdb.com/api.php?amount=10";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-//======================================================
 // Start Function
 
 
-//=======================================================
-// Hide/Display containers
+    $("#question_container").hide();
+    $("#end_container").hide();
 
-$("#question_container").hide();
-$("#end_container").hide();
+$("#startBtn").on("click", function(){
+
+    $("#start_container").hide();
+    $("#question_container").show();
+
+})
+}) 
+    
+
+
+
 
 //======================================================
 // Set up game timer
+var time = 30;
+
+function timer() {
+    clock = setInterval(countDown, 1000);
+    function countDown() {
+        if (time < 1) {
+            clearInterval(clock);
+            userTimeout();
+        }
+        if (time > 0) {
+            time--;
+        }
+        $("#timerDiv").html("<strong>" + time + "</strong>");
+        console.log(timer);
+    }
+}
 
 //=====================================================
-// Score Card Tracker
+// change question function
+
+//=====================================================
+// check answer function
 
 //=======================================================
 // Restart function
